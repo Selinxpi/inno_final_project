60f6e0256c9b66920e274c32ddaab3a5284bcaa2
Anastasija H <anastasija100991@gmail.com>
2022-07-11 20:23:16 +0100

    contact pagr and events

--- static/app.js
+++ static/app.js
@@ -28,13 +28,75 @@ function darkCheck(){
     let state = localStorage.getItem("state");
     if (state =="dark"){
         element.classList.toggle("dark-mode");
-    
-        for (const i of mainBox) {
-            i.classList.toggle("dark-modeb");
-        }
-    
-        for(const x of mainText) {
-            x.classList.toggle("dark-mode");
     }
+}
+
+function myFunction() {
+    var x = document.getElementById("divBea");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
     }
+}
+function myFunction2() {
+    var x = document.getElementById("hide_2");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
     }
+}
+function myFunction3() {
+    var x = document.getElementById("hide_3");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+}
+function myFunction4() {
+    var x = document.getElementById("hide_4");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction5() {
+    var x = document.getElementById("hide_5");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+}
+function myFunction6() {
+    var x = document.getElementById("hide");
+    if (x.style.display === "none") {
+    x.style.display = "block";
+    } else {
+    x.style.display = "none";
+    }
+}
+
+// function myFunction6() {
+
+//     $(document).ready(function() {
+//     $('.form-box1,.form-box2,.form-box3').hide();
+//     });
+
+
+//     $("#hide-text").click(function() {
+//         $(".form-box1").show();
+//     });
+
+//     $("#hide-text2").click(function() {
+//         $(".form-box2").show();
+//     });
+
+//     $("#hide-text3").click(function() {
+//         $(".form-box3").show();
+//     });
+// };
--- views.py
+++ views.py
@@ -5,3 +5,11 @@ my_view = Blueprint('my_view', __name__)
 @my_view.route('/')
 def index():
     return render_template("index.html")
+
+@my_view.route('/events')
+def events():
+    return render_template("events.html")
+
+@my_view.route('/contactus')
+def contactus():
+    return render_template("contactus.html")
