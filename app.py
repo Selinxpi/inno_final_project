543ba730b1d09f7b5de3610203ed2fa35605343f
Anastasija H <anastasija100991@gmail.com>
2022-07-14 17:36:55 +0100

    finished web

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.register_blueprint(my_view)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404error.html"), 404
+    return render_template("404.html"), 404
 
 if __name__=="__main__":
     app.run(debug=True, port=8000)
